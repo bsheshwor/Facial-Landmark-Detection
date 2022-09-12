69e928b56911fd7418402252943445bcac49aaf6
sajan491 <sajanmahat491@gmail.com>
2022-09-12 19:32:21 +0545

    implemented random crop and conversion of samples to tensors

--- data_load.py
+++ data_load.py
@@ -105,3 +105,49 @@ class Rescale(object):
         key_pts = key_pts * [new_w / w, new_h / h]
 
         return {'image': img, 'keypoints': key_pts}
+
+
+class RandomCrop(object):
+    """Crop randomly the image in a sample.
+    Args:
+        output_size (tuple or int): Desired output size. If int, square crop
+            is made.
+    """
+
+    def __init__(self, output_size):
+        assert isinstance(output_size, (int, tuple))
+        if isinstance(output_size, int):
+            self.output_size = (output_size, output_size)
+        else:
+            assert len(output_size) == 2
+            self.output_size = output_size
+
+    def __call__(self, sample):
+        image, key_pts = sample['image'], sample['keypoints']
+
+        h, w = image.shape[:2]
+        new_h, new_w = self.output_size
+
+        top = np.random.randint(0, h - new_h)
+        left = np.random.randint(0, w - new_w)
+
+        image = image[top: top + new_h,
+                      left: left + new_w]
+
+        key_pts = key_pts - [left, top]
+
+        return {'image': image, 'keypoints': key_pts}
+
+
+class ToTensor(object):
+    """Convert ndarrays in sample to Tensors."""
+
+    def __call__(self, sample):
+        image, key_pts = sample['image'], sample['keypoints']
+        if(len(image.shape) == 2):
+            image = image.reshape(image.shape[0], image.shape[1], 1)
+
+        image = image.transpose((2, 0, 1))
+        
+        return {'image': torch.from_numpy(image),
+                'keypoints': torch.from_numpy(key_pts)}
